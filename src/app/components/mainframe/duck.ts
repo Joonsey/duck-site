f7e6ccc8df1a5b54fdc9b5ef64c911a8a749b87d
Johannes <59510166+Joonsey@users.noreply.github.com>
2022-08-28 16:48:39 +0200

    started on website

---
+++ src/app/components/mainframe/duck.ts
@@ -0,0 +1,7 @@
+export interface IDuck {
+	duckNumber : string;
+	name : string;
+	age : number;
+	alcoholic : boolean;
+	color : number;
+}
---
+++ src/app/components/mainframe/mainframe.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { IDuck } from './duck';
+
+@Component({
+	selector: 'app-mainframe',
+	templateUrl: './mainframe.component.html',
+	styleUrls: ['./mainframe.component.css']
+})
+export class MainframeComponent implements OnInit {
+	ducks: IDuck[] = [];
+	api_root = "https://rubberduckapi.azurewebsites.net/Duck";
+
+	ngOnInit(): void {
+
+		this.ducks = this.getAllDucks()
+		const canvas: HTMLCanvasElement = document.getElementById('mainframe') as HTMLCanvasElement;
+
+		// draw something on canvas
+
+	}
+	getAllDucks(): IDuck[] {
+		let ducks: IDuck[]  = []
+		//ducks.push({name: 'jae', age: 3, alcoholic: false, duckNumber: 'pog', color: 2})
+
+		let response: Promise<Response> = fetch(this.api_root)
+			.then((response) => (response.json()))
+			.then((response) => response.forEach((r: any) => ducks.push(r)))
+
+		return ducks
+	}
+}
